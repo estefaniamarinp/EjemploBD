11731d82158b147c8a09b37a4df375f0654d30a1
Estudiante <estuam@UAM.LOCAL>
2017-03-10 20:27:42 -0500

    Se agregaron migraciones y modelos para Users, Profiles, Groups, Products

---
+++ database/migrations/2017_03_11_011538_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->text('address')->nullable();
+            $table->string('phone', 12)->nullable();
+            $table->string('city', 45)->nullable();
+            $table->string('country', 45)->nullable();
+            $table->string('website', 128)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
